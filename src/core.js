e04cf454249e8e9fe729487a5911974ceb607cc9
moonrailgun <moonrailgun@gmail.com>
2016-02-21 14:13:46 +0800

    事件常量
    ADD core
    ADD 事件常量

---
+++ src/core.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Chen on 2016-02-21.
+ */
+
+(function(window){
+    var ac = {
+        version: '1.0.0'
+    };
+
+    window.ac = ac;
+})(window);
---
+++ src/event.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Chen on 2016-02-21.
+ */
+
+(function(ac){
+    ac.event = {
+        BATTERY_LOW:'batterylow',
+        BATTERY_STATUE:'batterystatus',
+        KEY_BACK: 'keyback',
+        KEY_MENU: 'keymenu',
+        OFFLINE: 'offline',
+        ONLINE: 'online',
+        PAUSE: 'pause',
+        RESUME: 'resume',
+        SCROLL_TO_BOTTOM: 'scrolltobottom',
+        SHAKE: 'shake',
+        SWIPE_DOWN: 'swipedown',
+        SWIPE_LEFT: 'swipeleft',
+        SWIPE_RIGHT: 'swiperight',
+        SWIPE_UP: 'swipeup',
+        TAP:'tap',
+        VIEW_APPEAR:'viewappear',
+        VIEW_DISAPPEAR: 'viewdisappear',
+        NOTICE_CLICKED: 'noticeclicked',
+        APP_INTENT: 'appintent',
+        SMART_UPDATE_FINISH: 'smartupdatefinish'
+    }
+})(ac);
