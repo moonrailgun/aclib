be36a931ec401a0f4788c29d9f191830a31a966e
moonrailgun <moonrailgun@gmail.com>
2016-02-21 14:41:33 +0800

    应用常量
    ADD 应用常量

--- src/core.js
+++ src/core.js
@@ -4,7 +4,35 @@
 
 (function(window){
     var ac = {
-        version: '1.0.0'
+        acVersion: '1.0.0',
+
+        appId:api.appId,
+        appName : api.appName,
+        appVersion:api.appVersion,
+        systemType:api.systemType,
+        systemVersion: api.systemVersion,
+        version: api.version,
+        deviceId:api.deviceId,
+        deviceToken: api.deviceToken,
+        deviceModel:api.deviceModel,
+        deviceName:api.deviceName,
+        operator:api.operator,
+        connectionType:api.connectionType,
+        fullScreen:api.fullScreen,
+        screenWidth:api.screenWidth,
+        screenHeight:api.screenHeight,
+        winName:api.winName,
+        winWidth: api.winWidth,
+        winHeight:api.winHeight,
+        frameName:api.frameName,
+        frameWidth:api.frameWidth,
+        frameHeight:api.frameHeight,
+        pageParam:api.pageParam,
+        wgtParam:api.wgtParam,
+        appParam:api.appParam,
+        wgtRootDir:api.wgtRootDir,
+        fsDir:api.fsDir,
+        cacheDir:api.cacheDir
     };
 
     window.ac = ac;
