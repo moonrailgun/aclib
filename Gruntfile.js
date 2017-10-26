db55daefdb1fc02b70e22c9676fec3eff4b83a27
moonrailgun <moonrailgun@gmail.com>
2017-10-26 10:12:14 +0800

    update jsdoc

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,11 +26,20 @@ module.exports = function (grunt) {
                 src: 'src/**/*.js',
                 dest: 'dist/<%= pkg.name %> - <%= pkg.version%>.min.js'
             }
+        },
+        jsdoc : {
+            dist : {
+                src: ['src/*.js'],
+                options: {
+                    destination: 'doc'
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-jsdoc');
 
     // 默认被执行的任务列表。
     grunt.registerTask('default', ['uglify', 'concat']);
--- src/network.js
+++ src/network.js
@@ -3,16 +3,26 @@
  */
 
 (function (ac, window) {
+    /**
+     * @function 发送Ajax请求
+     * @param params {object} 参数
+     * @param callback {function} 回调
+     * @example
+     */
     ac.ajax = function (params, callback) {
         api.ajax(params, callback);
     };
 
+    /**
+     * @function 取消Ajax请求
+     * @param tag {object} 标识
+     */
     ac.cancelAjax = function (tag) {
         api.cancelAjax({tag: tag});
     };
 
     ac.download = function (params, callback) {
-        if (typeof params == 'string') {
+        if (typeof params === 'string') {
             api.download({
                 url: params
             }, callback);
